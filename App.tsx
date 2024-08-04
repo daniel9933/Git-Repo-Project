fa32e7e486622d5d4045e35bb9d7caf880a8b49a
daniel9933 <danielkolesnikov99@gmail.com>
2024-08-04 13:22:36 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './components/Welcome';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Welcome />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Welcome.tsx
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+
+export default function Welcome(){
+    fontsize:"20px";
+    return <Text>hello world</Text>
+}
