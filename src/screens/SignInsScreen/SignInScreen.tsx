9e7d1934a92f20296f3d5f5122ba9155ffae6e68
daniel9933 <danielkolesnikov99@gmail.com>
2024-08-06 20:09:26 +0300

    added scrollview

--- src/screens/SignInsScreen/SignInScreen.tsx
+++ src/screens/SignInsScreen/SignInScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View , Text , Image , StyleSheet, useWindowDimensions} from 'react-native';
+import { View , Text , Image , StyleSheet, useWindowDimensions, ScrollView} from 'react-native';
 import Logo from '../../../assets/images/Logo_1.png';
 import CustomInput from '../../components/CustomInput'
 import CustomButton from '../../components/CustomButton'
@@ -22,29 +22,32 @@ const SignInScreen = () => {
 
 
     return (
-        <View style = {styles.root}>
+        
+        <ScrollView showsVerticalScrollIndicator = {false}>
+           <View style = {styles.root}>
 
-            <Image source = {Logo}
-             style={[styles.logo, {height: height * 0.3}]} 
-             resizeMode="contain"/>
+                <Image source = {Logo}
+                style={[styles.logo, {height: height * 0.3}]} 
+                resizeMode="contain"/>
 
-            <CustomInput 
-            placeholder ='Username' 
-            value = {Username} 
-            setValue = {setUsername}
-            secureTextEntry = {false}
-            />
-            <CustomInput
-             placeholder ='Password' 
-             value = {password} 
-             setValue = {setPassword}
-             secureTextEntry = {true}
-            />
+                <CustomInput 
+                placeholder ='Username' 
+                value = {Username} 
+                setValue = {setUsername}
+                secureTextEntry = {false}
+                />
+                <CustomInput
+                placeholder ='Password' 
+                value = {password} 
+                setValue = {setPassword}
+                secureTextEntry = {true}
+                />
 
-            <CustomButton text = 'Sign In' onPress = {onSignInPressed} type = "PRIMARY"/>
+                <CustomButton text = 'Sign In' onPress = {onSignInPressed} type = "PRIMARY"/>
 
-            <CustomButton text = 'Forgot password?' onPress = {OnForgotPasswordPressed} type = "TERTIARY"/>
-        </View>
+                <CustomButton text = 'Forgot password?' onPress = {OnForgotPasswordPressed} type = "TERTIARY"/>
+            </View>
+        </ScrollView>
     )
 }
 
@@ -52,7 +55,7 @@ const styles = StyleSheet.create({
     root: {
         alignItems: 'center',
         padding: 20,
-
+        marginVertical:150,
     },
     logo: {
         width: '70%',
