30c34c89c74911f3d601e177e41c59eff99ef2bc
daniel9933 <danielkolesnikov99@gmail.com>
2024-10-23 18:33:29 +0300

    authenticate access token fucntion
    updated the authenticate access token function to use JOSE library as well as to decrypt. made some todos.

--- server/src/middlewares/authenticateToken.ts
+++ server/src/middlewares/authenticateToken.ts
@@ -1,40 +1,61 @@
 import { Request, Response, NextFunction } from 'express';
-import dotenv from 'dotenv';
+import dotenv, { decrypt } from 'dotenv';
+import { decryptToken } from '../utils/tokenUtils';
 import * as jose from 'jose';
 
 dotenv.config();
 
-const secretKey = process.env.SECRET_KEY;
-const encryptionKey = process.env.ENCRYPTION_KEY;
-if (!secretKey || !encryptionKey) throw new Error("KEY is not defined in the environment variables");
+const secretKey = new TextEncoder().encode(process.env.SECRET_KEY);
+if (!secretKey) throw new Error("KEY is not defined in the environment variables");
 
 
 interface AuthRequest extends Request {
     userId: string;
 };
 
-
-export const authenticateToken = (req : Request, res : Response, next : NextFunction) => {
-    const authHeader = req.headers['authorization'];
-    console.log(authHeader)
-    if (authHeader && typeof authHeader === 'string'){
-    const token = authHeader.split(' ')[1];
-    if (token){
-    try{
-        const decoded = jwt.verify(token, secretKey) as AuthRequest;
-        (req as AuthRequest).userId = decoded.userId
-        next();
-    }
-    catch (error)
+// todo: functions to verify access token, refresh token, 
+export const authenticateAccessToken = async (req : Request, res : Response, next : NextFunction) => {
+    // this function verifies the access token
+    if (req.headers['authorization'])
     {
-        res.status(401).send("Invalid Token");
+        try{    
+            const authHeader = await decryptToken(req.headers['authorization'])
+            console.log(authHeader)
+            if (authHeader && typeof authHeader === 'string'){
+                const token = authHeader.split(' ')[1];
+                if (token){
+                    try{
+                        const { payload } = await jose.jwtVerify(token, secretKey);
+                        if (typeof payload.userId === 'string'){
+                            (req as AuthRequest).userId = payload.userId
+                            next();
+                        }
+                        else{
+                            console.log("userId is invalid.")
+                            return res.status(401).send("userId is invalid.")
+                        }
+                    }
+                    catch (error)
+                    {
+                        console.log("Invalid Token", error)
+                        return res.status(401).send("Invalid Token");
+                    }
+                    }
+                else {
+                        return res.status(401).send("no token")
+                    }
+                }
+            }
+            catch (err){
+                console.log("Token decryption failed.", err);
+                return res.status(401).send("Token decryption failed.")
+            }
+        }
+        else {
+            return res.status(401).send("No authorization header");
+        }
     }
-    }
-    else {
-    res.status(401).send("no token")
-    }
-    }
-}
+
 
 // todo: change the verifaction process to jose
 // todo: whole verifaction process of the refresh token, 
